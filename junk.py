ce74dc551182e10e95516dafe51601ffd11f4ba7
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-09-17 22:13:33 +0530

    edited file

--- junk.py
+++ junk.py
@@ -6,5 +6,11 @@ def simple():
 def max():
     print("Function max is invoked")
 
+def acess():
+    s=input("Enter the device name")
+    if s=='samsung' :
+        d=input("Enter the passcode")
+        if d=='1234':
+
 simple()
 max()
