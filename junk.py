354d029771ae0764c99e4bebe07d0b1d87ff22be
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-09-17 22:17:08 +0530

    edited file

--- junk.py
+++ junk.py
@@ -9,12 +9,18 @@ def max():
 def acess():
     s=input("Enter the device name")
     if s=='samsung' :
-        d=input("Welcome samsung , Enter the passcode")
+        d=input("Welcome samsung , Enter Your passcode")
         if d=='1234':
             print("acces granted")
         else:
             print("acess denied")
     elif s=='apple':
+        c=input("Welcome apple , Enter Your passcode")
+        if c=='0001':
+            print("acces granted")
+        else:
+            print("acess denied")
+
 
 simple()
 max()
